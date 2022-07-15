ca9d5d27b6378ffd5c63369a53e4423a593e18ee
MJDKhalil <moammer.khalil@outlook.com>
2022-07-15 14:14:36 +0200

    added babel

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React, {Suspense} from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from "react-dom/client";
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import './i18n';
 
-  ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+  root.render(
     <React.StrictMode>
       <Suspense fallback={(<div className='index__loading'><h2>Loading...</h2></div>)}>
           <App />
       </Suspense>,
     </React.StrictMode>,
-    document.getElementById('root')
   );
 
 
