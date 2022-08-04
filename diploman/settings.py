ea0bf4ed79df165c06280cacf03c633cfce30554
MJDKhalil <moammer.khalil@outlook.com>
2022-08-04 17:28:26 +0200

    changed debug

--- diploman/settings.py
+++ diploman/settings.py
@@ -8,11 +8,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-28=380frm4rvaa-j!uc&99#5c1lsae@hvdao8(0l1%6qdkyaw7'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'www.diploman.co', 'diploman.co']
 
